c1a79f1a1eb704b0cde4975e63eabf319b5aa08d
Matt Burke <spraints@gmail.com>
2018-03-05 22:25:40 +0100

    finish the client side

--- client.go
+++ client.go
@@ -1,28 +1,41 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/spf13/pflag"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 
 	"github.com/spraints/friendly-guacamole/p"
 )
 
 func main() {
 	log.SetPrefix("[client] ")
-	work := 0
-	pflag.IntVarP(&work, "work", "s", work, "amount of time to sleep in the job")
+	var work int32
+	pflag.Int32VarP(&work, "work", "s", work, "amount of time to sleep in the job")
 	server := "127.0.0.1:55533"
 	pflag.StringVarP(&server, "server", "a", server, "server to connect to (default "+server+")")
 	pflag.Parse()
 
-	grpc, err := grpc.Dial(server, grpc.WithInsecure())
+	conn, err := grpc.Dial(server, grpc.WithInsecure())
 	perr(err)
-	defer grpc.Close()
-	client := p.NewExampleClient(grpc)
+	defer conn.Close()
+	client := p.NewExampleClient(conn)
 	perr(err)
-	log.Printf("todo: use %v", client)
+
+	ctx := context.Background()
+	ctx = metadata.AppendToOutgoingContext(ctx,
+		"request-id", fmt.Sprintf("req-%d", time.Now().Unix()),
+	)
+	res, err := client.DoSomeWork(ctx, &p.WorkRequest{Amount:work})
+	log.Printf("res: %#v", res)
+	if err != nil {
+		log.Printf("err: %s", err.Error())
+	}
 }
 
 func perr(err error) {
